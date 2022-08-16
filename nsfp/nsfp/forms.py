3cd22efad7300ded4d4be5b588785f4211e2b13a
ProfJAT <JoshuaT22@lakesideschool.org>
2022-08-16 14:23:33 -0700

    Styling and content modifications

--- nsfp/nsfp/forms.py
+++ nsfp/nsfp/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
+attrs = {
+    "class": "underline interest-input",
+    "autocomplete": "off"
+}
+
 class InterestForm(forms.Form):
-    business = forms.CharField(max_length=30, label="Business Name")
-    owner = forms.CharField(max_length=30, label="Owner Name")
-    email = forms.EmailField(max_length=30, label="Email For Contact")
-    city = forms.CharField(max_length=30, label="City")
-    current_site = forms.URLField(max_length=30, required=False, label="Current Site", initial="https://")
+    business = forms.CharField(max_length=30, widget=forms.TextInput(attrs={**attrs, **{"placeholder": "Business Name"}}))
+    owner = forms.CharField(max_length=30, widget=forms.TextInput(attrs={**attrs, **{"placeholder": "Owner Name"}}))
+    email = forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={**attrs, **{"placeholder": "Email For Contact"}}))
+    city = forms.CharField(max_length=30, widget=forms.TextInput(attrs={**attrs, **{"placeholder": "City"}}))
+    current_site = forms.URLField(max_length=30, required=False, widget=forms.TextInput(attrs={**attrs, **{"placeholder": "Current Site (If Applicable)"}}))
