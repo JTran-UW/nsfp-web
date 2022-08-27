ef4fe4cf7adb7707b993d64b6fae5f0aed871802
Joshua Tran <46096425+ProfJAT@users.noreply.github.com>
2022-08-27 00:37:35 -0700

    Update settings.py

--- nsfp/nsfp/settings.py
+++ nsfp/nsfp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$clt=j760z0^ru0*q_mgu995n6_6rk&u776hz!u4!4%rut06$e'
+SECRET_KEY = 'INSERT SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.159"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
